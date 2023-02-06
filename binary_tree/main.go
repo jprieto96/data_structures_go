fe07f57719f34d5f541b3657c6fb5e678033d2d1
Jose Prieto <joseprieto9657@gmail.com>
2023-02-06 23:19:41 +0100

    insert to binary tree implemented

--- binary_tree/main.go
+++ binary_tree/main.go
@@ -2,11 +2,8 @@ package main
 
 import "fmt"
 
-type any interface {
-}
-
 type Node struct {
-	value      *any
+	value      int
 	leftChild  *Node
 	rightChild *Node
 }
@@ -15,9 +12,31 @@ type BinaryTree struct {
 	root *Node
 }
 
-func (binaryTree *BinaryTree) insert(value *any) {
-	if binaryTree != nil {
+func (binaryTree *BinaryTree) Insert(value int) *BinaryTree {
+	if binaryTree.root == nil {
+		binaryTree.root = &Node{value: value, leftChild: nil, rightChild: nil}
+	} else {
+		binaryTree.root.insert(value)
+	}
+	return binaryTree
+}
 
+func (node *Node) insert(value int) {
+	if node == nil {
+		return
+	}
+	if value <= node.value {
+		if node.leftChild == nil {
+			node.leftChild = &Node{value: value, leftChild: nil, rightChild: nil}
+		} else {
+			node.leftChild.insert(value)
+		}
+	} else {
+		if node.rightChild == nil {
+			node.rightChild = &Node{value: value, leftChild: nil, rightChild: nil}
+		} else {
+			node.rightChild.insert(value)
+		}
 	}
 }
 
